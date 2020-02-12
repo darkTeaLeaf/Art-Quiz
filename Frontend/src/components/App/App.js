d5c4b059448fcb164cf50818c5f508d12248cb3b
Nagellan <31626879+Nagellan@users.noreply.github.com>
2020-02-12 18:46:13 +0300

    Restructure frontend folders + Add icon;

---
+++ Frontend/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="App">
+      <img src={process.env.PUBLIC_URL + "logo512.png"} alt="logo"></img>
+    </div>
+  );
+}
+
+export default App;
