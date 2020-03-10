aa55dbb7764976cd7f57dfb7ed8a0eae23865af8
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2020-03-10 18:08:45 +0300

    ID with a token

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -21,7 +21,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 
     path('', include('quiz.urls')),
     path('', include('user.urls')),
