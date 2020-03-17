f22fc6c3d379bce712284bcdbb5011b86ab43684
Nagellan <31626879+Nagellan@users.noreply.github.com>
2020-03-17 22:23:27 +0300

    Add signUp form fields

--- Frontend/src/components/AuthForm/AuthForm.js
+++ Frontend/src/components/AuthForm/AuthForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { signIn } from "../../actions/accountActions";
+import { signIn, /*signUp*/ } from "../../actions/accountActions";
 import "./AuthForm.css";
 
 const SignIn = ({ signIn, toggleAuthFormActive }) => {
@@ -19,13 +19,16 @@ const SignIn = ({ signIn, toggleAuthFormActive }) => {
 
   return (
     <div className="sign-in">
-      <button className="close" onClick={() => {toggleAuthFormActive(false)}}>✕</button>
       <h2>Sign in</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="username" ref={register({ required: true })} />
+        <input type="text" name="username" ref={register({ required: true })} />
         {errors.username && <span>This field is required</span>}
 
-        <input name="password" ref={register({ required: true })} />
+        <input
+          type="password"
+          name="password"
+          ref={register({ required: true })}
+        />
         {errors.password && <span>This field is required</span>}
 
         <input type="submit" />
@@ -34,13 +37,66 @@ const SignIn = ({ signIn, toggleAuthFormActive }) => {
   );
 };
 
-const SignUp = () => {
-  return <div className="sign-up"></div>;
+const SignUp = ({ /*signUp,*/ toggleAuthFormActive }) => {
+  const { register, handleSubmit, errors } = useForm();
+  const history = useHistory();
+
+  const onSubmit = async credentials => {
+    // const status = await signUp(credentials);
+    // if (status === "success") {
+    //   history.push("/account");
+    //   toggleAuthFormActive(false);
+    // }
+  };
+
+  return (
+    <div className="sign-up">
+      <h2>Sign up</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="file" name="username" ref={register({ required: true })} />
+        {errors.username && <span>This field is required</span>}
+
+        <input type="text" name="username" ref={register({ required: true })} />
+        {errors.username && <span>This field is required</span>}
+
+        <input
+          type="text"
+          name="firstName"
+          ref={register({ required: true })}
+        />
+        {errors.firstName && <span>This field is required</span>}
+
+        <input type="text" name="lastName" ref={register({ required: true })} />
+        {errors.lastName && <span>This field is required</span>}
+
+        <input type="email" name="email" ref={register({ required: true })} />
+        {errors.email && <span>This field is required</span>}
+
+        <input
+          type="password"
+          name="password"
+          ref={register({ required: true })}
+        />
+        {errors.password && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 const AuthForm = ({ signIn, toggleAuthFormActive }) => {
   return (
     <div id="AuthForm">
+      <button
+        className="close"
+        onClick={() => {
+          toggleAuthFormActive(false);
+        }}
+      >
+        ✕
+      </button>
+
       <SignIn signIn={signIn} toggleAuthFormActive={toggleAuthFormActive} />
       <SignUp />
     </div>
@@ -48,7 +104,8 @@ const AuthForm = ({ signIn, toggleAuthFormActive }) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-  signIn: credentials => dispatch(signIn(credentials))
+  signIn: credentials => dispatch(signIn(credentials)),
+  // signUp: credentials => dispatch(signUp(credentials))
 });
 
 export default connect(null, mapDispatchToProps)(AuthForm);
