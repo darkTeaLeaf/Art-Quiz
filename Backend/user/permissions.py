b8b22b32a630db20ceb35e6109b71ba7634414f3
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2020-03-18 15:41:00 +0300

    Change nested json

--- Backend/user/permissions.py
+++ Backend/user/permissions.py
@@ -16,7 +16,7 @@ class UserPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Deny actions on objects if the user is not authenticated
 
-        if view.action in ['retrieve']:
+        if view.action in ['list', 'create', 'retrieve']:
             return True
 
         if not request.user.is_authenticated:
