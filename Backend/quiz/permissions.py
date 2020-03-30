38ea562a5e6a8936b630fac687a6c07ecdaa6f59
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2020-03-30 23:07:40 +0300

    Fix paintings permissions

--- Backend/quiz/permissions.py
+++ Backend/quiz/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class QuizPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if view.action in ['list', 'retrieve']:
+        if view.action in ['list', 'retrieve', 'random', 'variants']:
             return True
         elif view.action in ['create', 'update', 'partial_update', 'destroy']:
             return request.user.is_staff
@@ -12,7 +12,7 @@ class QuizPermission(permissions.BasePermission):
             return False
 
     def has_object_permission(self, request, view, obj):
-        if view.action in ['list', 'retrieve']:
+        if view.action in ['list', 'retrieve', 'random', 'variants']:
             return True
         elif view.action in ['create', 'update', 'partial_update', 'destroy']:
             return request.user.is_staff
