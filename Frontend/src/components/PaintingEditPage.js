802a08a3dea787c0e8d6010b802a4791701de00a
Nagellan <31626879+Nagellan@users.noreply.github.com>
2020-05-10 18:48:00 +0300

    Add form fields to PaintingEditPage

--- Frontend/src/components/PaintingEditPage.js
+++ Frontend/src/components/PaintingEditPage.js
@@ -1,18 +1,80 @@
 import React from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 import Container from "../components/UI/Container";
 import Title from "../components/UI/Title";
+import Input from "../components/UI/Input";
+import Button from "../components/UI/Button";
 
 const Layout = styled.div`
   width: 100%;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    margin-bottom: 12px;
+  }
+`;
+
+const UpdateButton = styled(Button)`
+  margin-top: 30px;
+`;
+
 const PaintingEditPage = ({ data }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Layout>
       <Container>
         <Title bold>{data.name}</Title>
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            placeholder="Painting name"
+            type="text"
+            name="name"
+            defaultValue={data.name}
+            register={register}
+            borderless
+          />
+
+          <Input
+            placeholder="Author"
+            type="text"
+            name="author"
+            defaultValue={data.author}
+            register={register}
+            borderless
+          />
+
+          <Input
+            placeholder="Year"
+            type="text"
+            name="year"
+            defaultValue={data.year}
+            register={register}
+            borderless
+          />
+
+          <Input
+            placeholder="Style"
+            type="text"
+            name="style"
+            defaultValue={data.style}
+            register={register}
+            borderless
+          />
+
+          <UpdateButton type="submit">Update</UpdateButton>
+        </Form>
       </Container>
     </Layout>
   );
--- Frontend/src/components/UI/Input.js
+++ Frontend/src/components/UI/Input.js
@@ -4,14 +4,14 @@ import styled from "styled-components";
 const Input = styled.div`
   font-family: Judson;
   font-weight: 700;
-  font-size: ${props => props.fontSize};
+  font-size: ${(props) => props.fontSize};
   color: black;
   text-decoration: unset;
 
   & input {
     width: 100%;
     padding: 10px;
-    border: 5px solid black;
+    border: ${(props) => (props.borderless ? 0 : 5)}px solid black;
     font-family: Raleway;
     font-style: normal;
     font-weight: normal;
@@ -24,19 +24,22 @@ export default ({
   placeholder,
   type,
   name,
+  defaultValue,
   required = false,
   register,
-  errors
+  borderless,
+  errors,
 }) => {
   return (
-    <Input className="input-wrapper">
+    <Input className="input-wrapper" borderless={borderless}>
       <input
         type={type}
         name={name}
+        defaultValue={defaultValue}
         ref={register({ required })}
         placeholder={placeholder}
       />
-      {errors[name] && <div className="error">* Required</div>}
+      {errors && errors[name] && <div className="error">* Required</div>}
     </Input>
   );
 };
