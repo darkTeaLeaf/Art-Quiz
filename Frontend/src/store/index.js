c615df96b01726a49e2349745302e0e665444966
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2020-05-19 17:47:13 +0300

    Delete frontend

--- Frontend/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { rootReducer } from "../reducers";
-import thunk from "redux-thunk";
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
