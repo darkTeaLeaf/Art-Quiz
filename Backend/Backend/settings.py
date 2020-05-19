c0d4457b03896c610215126f81db421356b9f905
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2020-05-19 18:57:06 +0300

    Files

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'nameless-beyond-79364.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 # Application definition
 
